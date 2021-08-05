69b39748b755f7d8e7ee40e28a7b086192bf6317
Eric Weidow <eric@weidow.se>
2021-08-05 17:08:47 +0200

    solving algorithm and UI
    Added the SolveProblem script as well as Coroutines to update each frame. Also added UI to show different values

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private GameObject Indice;
+    [SerializeField] private Text descriptionText;
+    public List<GameObject> Indices { get; private set; }
+
+    private readonly int rayLength = 10;
+    
+    void Start()
+    {
+        Indices = new List<GameObject>();
+        UpdateIndices();
+    }
+
+    // Handle adding more indices and clicking existing ones
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            HandleRayCast(0);
+        }
+
+        else if (Input.GetMouseButtonDown(1))
+        {
+            HandleRayCast(1);
+        }
+    }
+
+    private void HandleRayCast(int mouseButtonPressed)
+    {
+        // Ray and hit from where the camera is to where the screen is
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction, rayLength);
+
+        // Debug
+        Debug.DrawRay(ray.origin, ray.direction * rayLength, Color.green, 10);
+
+        // Create indice if no collider is where the left mouse button is clicked
+        if (mouseButtonPressed == 0 && hit.collider == null)
+        {
+            Vector3 pos = Input.mousePosition;
+            pos.z = 10;
+            pos = Camera.main.ScreenToWorldPoint(pos);
+            Instantiate(Indice, pos, Indice.transform.rotation);
+            UpdateIndices();
+        }
+        // Remove indice if there is one where the right mouse button is clicked
+        else if (mouseButtonPressed == 1 && hit.collider != null && hit.collider.gameObject.CompareTag("Indice"))
+        {
+            Destroy(hit.collider.gameObject);
+            Indices.Remove(hit.collider.gameObject);
+        }
+    }
+
+    // Update the list by looping through all Indices and only adding the newest Indice
+    private void UpdateIndices()
+    {
+        GameObject[] tempList = GameObject.FindGameObjectsWithTag("Indice");
+
+        for(int i = 0; i < tempList.Length; i++)
+        {
+            if (!Indices.Contains(tempList[i]))
+            {
+                Indices.Add(tempList[i]);
+            }
+        }
+
+        descriptionText.text = "Solving " + Indices.Count + " point problem";
+    }
+}
--- Assets/Scripts/SolveProblem.cs
+++ Assets/Scripts/SolveProblem.cs
@@ -1,75 +1,154 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SolveProblem : MonoBehaviour
 {
-    [SerializeField] private GameObject Indice;
-    [SerializeField] private List<GameObject> Indices;
-
-    private int rayLength = 10;
+    // The game manager and text
+    private GameManager gameManager;
+    [SerializeField] private Text completionText;
     
-    // Start is called before the first frame update
+    // Indices for index and gameobjects
+    private int[] indices;
+    private GameObject[] gameObjectIndices;
+    
+    // The current best indice combination
+    private int[] bestTourIndices;
+    private float bestTourDst;
+
+    // The ratio of completed solutions compared to the total
+    private float currentSolution;
+    private float totalSolutions;
+
+    // The time for starting solve
+    private float startTime;
+    private float lastFrame;
+    private bool isStarted = false;
+
     void Start()
     {
-        Indices = new List<GameObject>();
-        UpdateIndices();
+        // Finding the game manager
+        gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
     }
 
-    // Handle adding more indices and clicking existing ones
     void Update()
-    {
-        if (Input.GetMouseButtonDown(0))
+    {   
+        if(isStarted)
         {
-            HandleRayCast(0);
+            UpdateText();
         }
 
-        else if (Input.GetMouseButtonDown(1))
+        if (currentSolution == totalSolutions)
         {
-            HandleRayCast(1);
+            isStarted = false;
         }
     }
 
-    private void HandleRayCast(int mouseButtonPressed)
+    public void Solve()
     {
-        // Ray and hit from where the camera is to where the screen is
-        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction, rayLength);
-
-        // Debug
-        Debug.DrawRay(ray.origin, ray.direction * rayLength, Color.green, 10);
-
-        // Create indice if no collider is where the left mouse button is clicked
-        if (mouseButtonPressed == 0 && hit.collider == null)
+        // Intialize and reset currentSolutions, totalSolutions, indices and gameObjectIndices
+        int count = gameManager.Indices.Count;
+        indices = new int[count];
+        bestTourIndices = new int[count];
+        startTime = Time.time;
+        isStarted = true;
+        
+        for (int i = 0; i < count; i++)
         {
-            Vector3 pos = Input.mousePosition;
-            pos.z = 10;
-            pos = Camera.main.ScreenToWorldPoint(pos);
-            Instantiate(Indice, pos, Indice.transform.rotation);
-            UpdateIndices();
+            indices[i] = i;
+            bestTourIndices[i] = i;
         }
-        // Remove indice if there is one where the right mouse button is clicked
-        else if (mouseButtonPressed == 1 && hit.collider != null)
+
+        gameObjectIndices = gameManager.Indices.ToArray();
+
+        bestTourDst = float.MaxValue;
+        currentSolution = 0;
+        totalSolutions = Factorial(count - 1) / 2;
+
+        /// Call with length - 1 to keep one element fixed in place. This avoids wasting time
+        /// evaluating tours that are identical except for beginning at a different point
+        GenerateSolutions(indices, indices.Length - 1);
+    }
+
+    // Heap's algoritm for generating all permutations
+    private void GenerateSolutions(int[] indices, int n)
+    {
+        if (n == 1)
         {
-            if (hit.collider.gameObject.CompareTag("Indice"))
+            StartCoroutine(EvaluateSolution());
+        } else
+        {
+            for (int i = 0; i < n; i++)
             {
-                Destroy(hit.collider.gameObject);
-                Indices.Remove(hit.collider.gameObject);
+                GenerateSolutions(indices, n - 1);
+                int swapIndex = (n % 2 == 0) ? i : 0;
+                (indices[swapIndex], indices[n - 1]) = (indices[n - 1], indices[swapIndex]);
             }
         }
     }
 
-    // Update the list by looping through all Indices and only adding the newest Indice
-    private void UpdateIndices()
-    {
-        GameObject[] tempList = GameObject.FindGameObjectsWithTag("Indice");
-
-        for(int i = 0; i < tempList.Length; i++)
+    private IEnumerator EvaluateSolution()
+    {   
+        // Check if one frame has passed
+        if (Time.time - 0.02 > lastFrame)
         {
-            if (!Indices.Contains(tempList[i]))
+            lastFrame = Time.time;
+            yield return new WaitForEndOfFrame();
+            Debug.Log(Time.time);
+        }
+        
+        // Ignore solutions which are just the reverse of another solution
+        if (indices[0] < indices[indices.Length - 2])
+        {
+            // Add to the current solution
+            currentSolution++;
+
+            // Calculate length of the path (including returning to start point)
+            float tourDst = 0;
+            for (int i = 0; i < indices.Length; i++)
             {
-                Indices.Add(tempList[i]);
+                // Starts from 0 --> 1, ends at indices.Length --> 0
+                int nextIndex = (i + 1) % indices.Length;
+                tourDst += LookUpDistance(indices[i], indices[nextIndex]);
+            }
+
+            // Save the path indices if this is the best solution found so far
+            if (tourDst < bestTourDst)
+            {
+                bestTourDst = tourDst;
+                
+                // Copy over one array to another
+                System.Array.Copy(indices, bestTourIndices, indices.Length);
             }
         }
+    }
+
+    private float LookUpDistance(int index1, int index2)
+    {
+        float dx = gameObjectIndices[index1].transform.position.x - gameObjectIndices[index2].transform.position.x;
+        float dy = gameObjectIndices[index1].transform.position.y - gameObjectIndices[index2].transform.position.y;
+        return Mathf.Sqrt(Mathf.Pow(dx, 2) + Mathf.Pow(dy, 2));
+    }
+
+    private int Factorial(int n)
+    {
+        int sum = 1;
+        
+        for (int i = n; i > 0; i--)
+        {
+            sum *= i;
+        }
+
+        return sum;
+    }
+
+    private void UpdateText()
+    {   
+        completionText.text = currentSolution + " / " + totalSolutions + "\n"
+                            + Math.Round(currentSolution / totalSolutions * 100, 2) + " % \n"
+                            + Math.Round(Time.time - startTime, 2) + " seconds \n"
+                            + Math.Round(bestTourDst, 2) + " km";
     }
 }
